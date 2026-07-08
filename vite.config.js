92bcd1b13f5f9adbd999d5762c2d499f34db0fb9
Ayomide Agbaje <agbajee835@gmail.com>
2026-07-08 10:53:26 +0100

    Update base path in Vite config and refresh metadata for dependencies

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/asoebi-payment-info/',
+  base: '/asoebi-payment-details/',
 })
